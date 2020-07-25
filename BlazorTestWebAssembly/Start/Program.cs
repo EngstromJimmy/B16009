0aed6475bb4e8e4fd2b45a5c62ecb41468f974dd
Jimmy Engström <jimmy@apeoholic.se>
2020-07-25 22:43:17 +0200

    Initial check in

---
+++ BlazorTestWebAssembly/Start/Program.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorTestWebAssembly
+{
+    //WASMProgram
+}
