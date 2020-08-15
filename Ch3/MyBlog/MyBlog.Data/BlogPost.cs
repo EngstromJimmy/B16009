19a9aa2d5bafc3f15e93214ebfb4626db332c843
Jimmy Engström <jimmy@apeoholic.se>
2020-08-15 18:27:22 +0200

    Added data projects and classes

---
+++ Ch3/MyBlog/MyBlog.Data/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyBlog.Data
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime PublishDate { get; set; }
+        public Category Category { get; set; }
+        public virtual ICollection<Tag> Tags { get; set; }
+    }
+}
---
+++ Ch3/MyBlog/MyBlog.Data/Tag.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MyBlog.Data
+{
+    public class Tag
+    {
+    }
+}
