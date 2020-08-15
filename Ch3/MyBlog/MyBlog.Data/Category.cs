218c6a1d72b08fc61e0797ea389964bce0bbc5ef
Jimmy Engström <jimmy@apeoholic.se>
2020-08-15 18:44:49 +0200

    Added code for tag and category

--- Ch3/MyBlog/MyBlog.Data/Category.cs
+++ Ch3/MyBlog/MyBlog.Data/Category.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace MyBlog.Data
 {
     public class Category
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<BlogPost> BlogPosts { get; set; }
     }
 }
--- Ch3/MyBlog/MyBlog.Data/Tag.cs
+++ Ch3/MyBlog/MyBlog.Data/Tag.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyBlog.Data
 {
     public class Tag
     {
+        public int Id {get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<BlogPost> BlogPosts { get; set; }
     }
 }
