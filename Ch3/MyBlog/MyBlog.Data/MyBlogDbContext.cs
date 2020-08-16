583da457c4ab41c99f64cba6d2a9205cc67f4847
Jimmy Engström <jimmy@apeoholic.se>
2020-08-16 17:07:26 +0200

    Moved files around

---
+++ Ch3/MyBlog/MyBlog.Data/MyBlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyBlog.Data
+{
+    public class MyBlogDbContext : DbContext
+    {
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
