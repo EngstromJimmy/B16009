c6e5c7a1fc0ea3392dbfad8a297694feb631059c
Jimmy Engström <jimmy@apeoholic.se>
2020-10-25 18:49:41 +0100

    Added CH6

---
+++ Ch6/MyBlog/MyBlogServerSide/Form/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlogServerSide.Form
+{
+    //<PersonClass>
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    //</PersonClass>
+}
