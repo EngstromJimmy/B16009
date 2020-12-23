aa31075bc9504fe1e31f3fdd709d7fb3ce453276
Jimmy Engström <jimmy@apeoholic.se>
2020-12-23 20:50:09 +0100

    added constructor

---
+++ Ch7/MyBlog/MyBlogWebAssembly/Server/Controllers/MyBlogApiController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.Data.Interfaces;
+using MyBlog.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlogWebAssembly.Server.Controllers
+{
+    public class MyBlogApi:Controller
+    {
+        //<Constructor>
+        public IMyBlogApi api { get; set; }
+        public MyBlogApi(IMyBlogApi api)
+        {
+            this.api = api;
+        }
+        //</Constructor>
+      
+    }
+}
