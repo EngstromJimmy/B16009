12192f25d7a4866289e677fb1247f6e45e3494f4
Jimmy Engström <jimmy@apeoholic.se>
2020-12-25 22:26:08 +0100

    Added HttpClientExtensions

--- Ch7/MyBlog/MyBlog.Data/MyBlogApiClientSide.cs
+++ Ch7/MyBlog/MyBlog.Data/MyBlogApiClientSide.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using MyBlog.Data.Interfaces;
 using System.Net.Http;
 using MyBlog.Data.Models;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 //</using>
 namespace MyBlog.Data
 {
@@ -18,6 +20,100 @@ namespace MyBlog.Data
         {
             this.httpclient = httpclient;
         }
+
+        //<Blogpost>
+        public async Task<BlogPost> GetBlogPostAsync(int id)
+        {
+            return await httpclient.GetFromJsonAsync<BlogPost>($"BlogPosts/{id}");
+        }
+
+        public async Task<int> GetBlogPostCountAsync()
+        {
+            return await httpclient.GetFromJsonAsync<int>("BlogPostsCount");
+        }
+
+        public async Task<List<BlogPost>> GetBlogPostsAsync(int numberofposts, int startindex)
+        {
+            return await httpclient.GetFromJsonAsync<List<BlogPost>>($"BlogPosts?numberofposts={numberofposts}&startindex={startindex}");
+        }
+
+        public async Task<BlogPost> SaveBlogPostAsync(BlogPost item)
+        {
+            try
+            {
+                var response= await httpclient.DeleteAsync<BlogPost>("BlogPosts",item);
+            }
+            catch (AccessTokenNotAvailableException exception)
+            {
+                exception.Redirect();
+            }
+        }
+
+        public async Task DeleteBlogPostAsync(BlogPost item)
+        {
+            try
+            {
+                var response = await httpclient.PutAsJsonAsync<BlogPost>("BlogPosts", item);
+            }
+            catch (AccessTokenNotAvailableException exception)
+            {
+                exception.Redirect();
+            }
+        }
+        //</Blogpost>
+
+
+
+
+
+
+
+
+        public Task DeleteCategoryAsync(Category item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteTagAsync(Tag item)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public Task<List<Category>> GetCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> GetCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag> GetTagAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Tag>> GetTagsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public Task<Category> SaveCategoryAsync(Category item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag> SaveTagAsync(Tag item)
+        {
+            throw new NotImplementedException();
+        }
         //</Constructor>
+
+
     }
 }
