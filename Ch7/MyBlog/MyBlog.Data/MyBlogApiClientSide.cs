e7871eb54c869d3680a86325cc78ffe6bd27ddcf
Jimmy Engström <jimmy@apeoholic.se>
2020-12-25 21:55:14 +0100

    Added MyBlogApiClientSide

---
+++ Ch7/MyBlog/MyBlog.Data/MyBlogApiClientSide.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//<using>
+using MyBlog.Data.Interfaces;
+using System.Net.Http;
+using MyBlog.Data.Models;
+//</using>
+namespace MyBlog.Data
+{
+    public class MyBlogApiClientSide:IMyBlogApi
+    {
+        //<Constructor>
+        HttpClient httpclient;
+        public MyBlogApiClientSide(HttpClient httpclient)
+        {
+            this.httpclient = httpclient;
+        }
+        //</Constructor>
+    }
+}
