c222fc965e17622b3d9cece3bc903ff95405c164
Jimmy Engström <jimmy@apeoholic.se>
2020-12-29 21:48:00 +0100

    moved the file

--- Ch7/MyBlog/MyBlogWebAssembly/Client/Authentication/RoleAccountClaimsPrincipalFactory.cs
+++ Ch7/MyBlog/MyBlogWebAssembly/Client/Authentication/RoleAccountClaimsPrincipalFactory.cs
@@ -5,7 +5,7 @@ using System.Security.Claims;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace MyBlogServerSide.Authentication
+namespace MyBlogWebAssembly.Client.Authentication
 {
     public class RoleAccountClaimsPrincipalFactory : AccountClaimsPrincipalFactory<RemoteUserAccount>
     {
