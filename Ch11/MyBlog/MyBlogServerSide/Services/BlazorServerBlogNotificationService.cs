7036cbbfd576ebb0724a6367fdd1d72340b70a74
Jimmy Engström <jimmy@apeoholic.se>
2021-01-24 21:15:40 +0100

    added service

---
+++ Ch11/MyBlog/MyBlogServerSide/Services/BlazorServerBlogNotificationService.cs
@@ -0,0 +1,18 @@
+﻿using MyBlog.Data.Models;
+using MyBlog.Data.Shared.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace MyBlogServerSide.Services
+{
+    public class BlazorServerBlogNotificationService : IBlogNotificationService
+    {
+        public Action<BlogPost> BlogPostChanged { get; set; }
+
+        public Task SendNotification(BlogPost post)
+        {
+            BlogPostChanged?.Invoke(post);
+            return Task.CompletedTask;
+        }
+    }
+}
