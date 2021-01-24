17535563cfcca5e10a8b831f08bbbe8b307c4a24
Jimmy Engström <jimmy@apeoholic.se>
2021-01-24 20:54:52 +0100

    Removed namespaces

--- Ch11/MyBlog/MyBlog.Data.Shared/Interfaces/IBlogNotificationService.cs
+++ Ch11/MyBlog/MyBlog.Data.Shared/Interfaces/IBlogNotificationService.cs
@@ -1,8 +1,5 @@
 ﻿using MyBlog.Data.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MyBlog.Data.Shared.Interfaces
