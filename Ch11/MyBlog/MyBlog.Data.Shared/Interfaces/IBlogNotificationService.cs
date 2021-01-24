673e960f50972ddff96e16d7ea5782096f86ee76
Jimmy Engström <jimmy@apeoholic.se>
2021-01-24 20:54:21 +0100

    Added service

---
+++ Ch11/MyBlog/MyBlog.Data.Shared/Interfaces/IBlogNotificationService.cs
@@ -0,0 +1,15 @@
+﻿using MyBlog.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Data.Shared.Interfaces
+{
+    public interface IBlogNotificationService
+    {
+        Action<BlogPost> BlogPostChanged { get; set; }
+        Task SendNotification(BlogPost post);
+    }
+}
